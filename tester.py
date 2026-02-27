4b26fb0b94316f72dffc0abb0027e605c1d02c28
Aji-Piyu <altheajaneablola@gmail.com>
2026-02-27 13:20:14 +0800

    added tester.py

--- tester.py
+++ tester.py
@@ -1,8 +1,8 @@
-# pyright: strict
+# pyright : strict
 
-from common_types import WinConditionType, TokenPhysicsType
+from common_types import WinconditionType, TokenPhysicsType, Player
 from model import ConnectTacToeModel
 
-
 def make(win_condition_type: WinConditionType, token_physics_type: TokenPhysicsType) -> ConnectTacToeModel:
-    ...
+    newmodel = ConnectTacToeModel([Player.P1,Player.P2],win_condition_type,token_physics_type)
+    return model

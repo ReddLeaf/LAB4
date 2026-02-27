b62db48cb0270bebcbd2639fbeb7b27f5ba5e9d6
Aji-Piyu <altheajaneablola@gmail.com>
2026-02-27 13:16:16 +0800

    controller added

---
+++ controller.py
@@ -0,0 +1,25 @@
+# pyright : strict
+
+from model  import ConnectTacToeView,ConnectTacToeModel
+
+
+class ConnectTacToeController:
+
+    def __init__(self,view:ConnectTacToeView,model:ConnectTacToeModel)
+        self._view = view
+        self._model = model
+
+    def run(self):
+        model = self._model
+        view = self._view
+
+        while not model.is_game_done:
+            view.display_grid()
+            view.show_current_player(model.current_player)
+        # ask for input
+            i,j = float('inf'), float('inf')
+            while not choose_cell(i,j):
+                i,j = view.ask_for_ij(model.row_count,model.col_count)
+            model.advance_turn()
+        view.show_winner(model.winner)
+        

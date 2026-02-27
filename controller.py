e86db6f54b94ed2e310f394dffc065a71255dfef
Virgil Simon Deola <virgilsimondeola@gmail.com>
2026-02-27 13:36:46 +0800

    Control fixed

--- controller.py
+++ controller.py
@@ -1,12 +1,12 @@
 # pyright : strict
 
 from model  import ConnectTacToeModel
-from view import ConnectTacToeViev
+from view import ConnectTacToeView
 
 
 class ConnectTacToeController:
 
-    def __init__(self,view:ConnectTacToeView,model:ConnectTacToeModel)
+    def __init__(self,view:ConnectTacToeView,model:ConnectTacToeModel):
         self._view = view
         self._model = model
 
@@ -16,9 +16,11 @@ class ConnectTacToeController:
         while not model.is_game_done:
             view.display_grid(model.grid)
             view.show_current_player(model.current_player)
-            i,j = float('inf'), float('inf')
-            while not choose_cell(i,j):
+            i,j = -1, -1
+            while True:
                 i,j = view.ask_for_ij(model.row_count,model.col_count)
+                if model.choose_cell(i, j):
+                    break
             model.advance_turn()
         view.show_winner(model.winner,model.grid)
         

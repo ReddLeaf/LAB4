434668233424e266ea9556719990c12639172147
ReddLeaf <gchalili@up.edu.ph>
2026-02-27 13:18:52 +0800

    transferred win condition classes int a new file

--- model.py
+++ model.py
@@ -79,34 +79,3 @@ class ConnectTacToeModel:
             if (row, col) in self._player_tokens[p]:
                 return p
         return None
-
-
-class WinConditions:
-    def is_game_done(self, grid: Sequence[list[str]], token: str) -> bool:
-        ...
-
-class BasicTicTacToe(WinConditions):
-    def is_game_done(self, grid: Sequence[list[str]], token: str) -> bool:
-        for row in grid:
-            counter = 0
-            for col in row:
-                if col == token:
-                    counter += 1
-                    if counter == 3:
-                        return True
-
-        for col in range(len(grid[0])):
-            counter = 0 
-            for row in grid:
-                if row[col] == token:
-                    counter += 1
-                    if counter == 3:
-                        return True
-        
-        return False
-
-class NotConnectFour(WinConditions):
-    ...
-
-class TicTacTOe(WinConditions):
-    ...
